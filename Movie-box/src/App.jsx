ece0a95592bff9fed0edeb548680a2849edeb532
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2023-09-12 15:07:12 +0100

    environment setup

---
+++ Movie-box/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
+
+import Home from './Components/Home';
+import Movie from './Components/Movie';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Home/>}>
+      <Route path="movie" element={<Movie />} />
+    </Route>
+  )
+)
+function App({routes}) {
+
+  return (
+    <>
+      <div className="the-body">
+      <RouterProvider router={router}/>
+      </div>
+    </>
+  )
+}
+
+export default App
